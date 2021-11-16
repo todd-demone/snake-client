4470425d259a0eeb2b4623b9d0ab51cf0d617eb0
Todd Demone <tdemone1@gmail.com>
2021-11-16 15:27:42 +0000

    move connect function to client.js; export connect function using es6 shorthand

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log('Connected to server.');
+  });
+  conn.on('ready', () => {
+    console.log('Socket is ready to be used.');
+  });
+  conn.on('data', (data) => {
+    console.log('Data received from server:', data);
+  });
+  conn.on('end', () => {
+    console.log('Server has signaled end of transmission.');
+  });
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,29 +1,4 @@
 const net = require('net');
+const connect = require('./client');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  conn.setEncoding("utf8");
-  conn.on('connect', () => {
-    console.log('connect event');
-  });
-  conn.on('ready', () => {
-    console.log('ready event');
-  });
-  conn.on('data', (data) => {
-    console.log('data event:', data);
-  });
-  conn.on('timeout', () => {
-    console.log('timeout event');
-  });
-  conn.on('end', () => {
-    console.log('end event');
-  });
-  return conn;
-};
-
-console.log('Connecting...');
 connect();
-console.log('after call to connect function');

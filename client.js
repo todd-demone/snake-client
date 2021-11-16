f46aea3aed1c0c9d45a1e37ca359ae4990d1b247
Todd Demone <tdemone1@gmail.com>
2021-11-16 15:47:26 +0000

    experiment with move commands via setTimeout() and setInterval()

--- client.js
+++ client.js
@@ -9,12 +9,16 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server.');
     conn.write('Name: TCD');
+    // setTimeout(() => { conn.write('Move: up'); }, 50);
+    // setTimeout(() => {conn.write('Move: up');}, 100);
+    // const timeoutObject = setInterval(() => {conn.write('Move: up');}, 50);
+    // clearInterval(timeoutObject)
   });
-  conn.on('ready', () => {
-    console.log('Socket is ready to be used.');
-  });
+  // conn.on('ready', () => {
+  //   console.log('Socket is ready to be used.');
+  // });
   conn.on('data', (data) => {
-    console.log('Data received from server:', data);
+    console.log('Server says:', data);
   });
   conn.on('end', () => {
     console.log('Server has signaled end of transmission.');

81fd1afee41310bee0178b384e47d832de376d54
Todd Demone <tdemone1@gmail.com>
2021-11-16 17:30:10 +0000

    create constants file; refactor client.js to no longer hard code IP, PORT and INITIALS; refactor input.js to no longer hard code key values and messages

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = 'localhost';
+const PORT = 50541;
+const INITIALS = 'TCD';
+const MESSAGES = {
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+  g: 'Say: Hello everyone!',
+  b: 'Say: Gotta run. Bye!',
+};
+
+module.exports = {
+  IP,
+  PORT,
+  INITIALS,
+  MESSAGES
+};

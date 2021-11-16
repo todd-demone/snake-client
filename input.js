da7a2b0fe1017797073290469f3252aa2a372361
Todd Demone <tdemone1@gmail.com>
2021-11-16 16:39:55 +0000

    add to input event handler the ability to send messages to fellow gamers on server.

--- input.js
+++ input.js
@@ -14,18 +14,18 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log('Exiting game.');
     process.exit();
-  }
-  if (key === 'w') {
+  } else if (key === 'w') {
     connection.write('Move: up');
-  }
-  if (key === 'a') {
+  } else if (key === 'a') {
     connection.write('Move: left');
-  }
-  if (key === 's') {
+  } else if (key === 's') {
     connection.write('Move: down');
-  }
-  if (key === 'd') {
+  } else if (key === 'd') {
     connection.write('Move: right');
+  } else if (key === 'h') {
+    connection.write('Say: Hi everyone!');
+  } else if (key === 'z') {
+    connection.write('Say: Gotta run. Bye!');
   }
 };
 

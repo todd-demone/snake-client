e782e5563d8da8d458454531935548044950cf01
Todd Demone <tdemone1@gmail.com>
2021-11-16 17:30:10 +0000

    create constants file; refactor client.js to no longer hard code IP, PORT and INITIALS; refactor input.js to no longer hard code key values and messages

--- client.js
+++ client.js
@@ -1,24 +1,23 @@
 const net = require('net');
 
+const {
+  IP,
+  PORT,
+  INITIALS
+} = require('./constants');
+
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log('Successfully connected to game server.');
-    conn.write('Name: TCD');
-    // setTimeout(() => { conn.write('Move: up'); }, 50);
-    // setTimeout(() => {conn.write('Move: up');}, 100);
-    // const timeoutObject = setInterval(() => {conn.write('Move: up');}, 50);
-    // clearInterval(timeoutObject)
+    conn.write(`Name: ${INITIALS}`);
   });
-  // conn.on('ready', () => {
-  //   console.log('Socket is ready to be used.');
-  // });
   conn.on('data', (data) => {
-    console.log('Server says:', data);
+    console.log(`Server says: ${data}`);
   });
   conn.on('end', () => {
     console.log('Server has signaled end of transmission.');
--- input.js
+++ input.js
@@ -1,31 +1,29 @@
+const { MESSAGES } = require('./constants');
+
 let connection;
 
 const setupInput = function(conn) {
-  connection = conn; // we will use the conn object returned by the connect function to write to the server in handleUserInput().
+  // The conn parameter is an object that is created by connection() in client.js.
+  // and passed in to setupInput in the play.js file.
+  // The object is then assigned to the connection variable.
+  // It is used by the event handler function (handleUserInput()) to write to the server.
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput);
-  return stdin; // the stdin object that is returned to us allows us to listen for keyboard events and handle them.
+  // The stdin object that is returned below allows us to listen for keyboard events and handle them.
+  return stdin;
 };
 
+//  Event handler function
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log('Exiting game.');
     process.exit();
-  } else if (key === 'w') {
-    connection.write('Move: up');
-  } else if (key === 'a') {
-    connection.write('Move: left');
-  } else if (key === 's') {
-    connection.write('Move: down');
-  } else if (key === 'd') {
-    connection.write('Move: right');
-  } else if (key === 'h') {
-    connection.write('Say: Hi everyone!');
-  } else if (key === 'z') {
-    connection.write('Say: Gotta run. Bye!');
+  } else if (key in MESSAGES) {
+    connection.write(MESSAGES[key]);
   }
 };
 

8aea6e828858bb241dfc6a120430761add60d752
Todd Demone <tdemone1@gmail.com>
2021-11-16 16:22:16 +0000

    handle user input ie wasd movement keys; pass connection object to setupInput function so that, on wasd events. setupInput's event handler can write movement instructions to the server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn; // we will use the conn object returned by the connect function to write to the server in handleUserInput().
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -11,6 +14,18 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log('Exiting game.');
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
--- play.js
+++ play.js
@@ -3,5 +3,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting...');
-connect();
-setupInput();
+const conn = connect();
+setupInput(conn);

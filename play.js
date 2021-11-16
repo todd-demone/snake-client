7b6efcb06e1a764190a9a884c52fff3b188d1b19
Todd Demone <tdemone1@gmail.com>
2021-11-16 16:02:54 +0000

    add two functions to deal with user input: setupInput() and handleUserInput()

--- play.js
+++ play.js
@@ -1,4 +1,21 @@
-const net = require('net');
+// const net = require('net');
 const connect = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin; // the stdin object that is returned to us allows us to listen for keyboard events and handle them.
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('Exiting game');
+    process.exit();
+  }
+};
+
 connect();
+setupInput();

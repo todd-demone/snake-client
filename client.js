9ff208e08ad39a1fcd1633fd7f936e978fba8764
Todd Demone <tdemone1@gmail.com>
2021-11-17 16:49:49 +0000

    update comments

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const { IP, PORT, ENCODING, INITIALS } = require('./constants');
 
 /**
  * Establishes a connection with the game server.
- * @returns {Object} conn - Returns a connection object, i.e.,  an instance of the class Socket.
+ * @returns {Object} conn - Returns a connection object, i.e., an instance of the class Socket.
  */
 const connect = function() {
   const conn = net.createConnection({ host: IP, port: PORT });
